5949aa56aeed42af8ba14dfc359aef5744250244
CARICOM12 <0190a@ukr.net>
2020-05-02 20:00:51 +0300

    Add files via upload

---
+++ WhileTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WhileTest
+{
+    /*На вход идут числа
+     * На выход - их квадратные корни
+     * Как только найдено отрицательное число или 
+     * некорректный ввод числа - завершение программы
+     * 
+     * 1. Вводится число
+      2.1. Если число > 0 - вывод
+      2.2. Иначе - завершение
+      3. Переход к 1. */
+
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter a number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while(number > 0)
+            {
+                Console.WriteLine(Math.Sqrt(number) + " ");
+                Console.Write("Please enter a number ");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
